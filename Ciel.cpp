a0f2bf66b32adf003a4c239ea58e4c778bcb4dfa
emgrvl <81969308+emgrvl@users.noreply.github.com>
2021-04-12 19:37:13 +0200

    Update Ciel.cpp

--- Ciel.cpp
+++ Ciel.cpp
@@ -3,77 +3,40 @@
 #include "cmath"
 
 
-// ----------- P8: Ciel et Nuages ----------------
+// P8: Ciel et Nuages
 
-// calcul de la vitesse
-void CubedAir:: v_vent(){
-    for (int i(0); i < Nx; ++i) {
-        for (int j(0); i < Ny; ++j) {
-            for (int k(0); i < Nz; ++k) {
-                Cube[i][j][k].v_vent= ; // pas fini
-            }
-        }
-    }
-}
 // calcul de l'enthalpie
-void CubedAir:: h(){
-    for (int i(0); i < Nx; ++i) {
-        for (int j(0); i < Ny; ++j) {
-            for (int k(0); i < Nz; ++k) {
-                Cube[i][j][k].h = bernouilli - g*z - 1/2 * Cube[i][j][k].v_vent * Cube[i][j][k].v_vent ; // c'est quoi z?
-            }
-        }
-    }
+double CubedAir:: h(){
+    double h;
+    //return h = bernouilli - g* Z[k] - 1/2 * v_vent * v_vent ; // c'est quoi z? z_k?
 }
 // calcul de la température
-void CubedAir::T(){
-    for (int i(0); i < Nx; ++i) {
-        for (int j(0); i < Ny; ++j) {
-            for (int k(0); i < Nz; ++k) {
-                Cube[i][j][k].T = 2/7 * (M_airsec/R) * Cube[i][j][k].h ;
-            }
-        }
-    }
+double CubedAir::T(){
+    double T;
+    return T = 2/7 * (M_airsec/R) * h() ;
 }
 // calcul de la pression
-void CubedAir:: pression(){
-    for (int i(0); i < Nx; ++i) {
-        for (int j(0); i < Ny; ++j) {
-            for (int k(0); i < Nz; ++k) {
-                Cube[i][j][k].pression = P_inf * pow(T_inf, -7/2) * pow(Cube[i][j][k].T, 7/2);
-            }
-        }
-    }
+double CubedAir:: pression(){
+    double pression;
+    return pression = P_inf * pow(T_inf, -7/2) * pow(T(), 7/2);
 }
 // calcul de la pression partielle de vapeur d'eau
-void CubedAir:: p_eau(){
-    for (int i(0); i < Nx; ++i) {
-        for (int j(0); i < Ny; ++j) {
-            for (int k(0); i < Nz; ++k) {
-                Cube[i][j][k].p_eau = Cube[i][j][k].tau* Cube[i][j][k].pression / (M_eau/M_airsec+ Cube[i][j][k].tau) ;
-            }
-        }
-    }
+double CubedAir:: p_eau(){
+    double p_eau;
+    return p_eau = tau* pression() / (M_eau/M_airsec+ tau) ;
 }
 // calcul de la pression vapeur saturante d'eau
-void CubedAir:: p_rosee(){
-    for (int i(0); i < Nx; ++i) {
-        for (int j(0); i < Ny; ++j) {
-            for (int k(0); i < Nz; ++k) {
-                Cube[i][j][k].p_rosee = P_ref * exp(13.96 - 5208/Cube[i][j][k].T) ;
-            }
-        }
-    }
+double CubedAir:: p_rosee(){
+    double p_rosee;
+    return p_rosee = P_ref * exp(13.96 - 5208/T()) ;
 }
-// determine s'il y a formation de nuage ou pas
-void CubedAir:: etat(){
-    for (int i(0); i < Nx; ++i) {
-        for (int j(0); i < Ny; ++j) {
-            for (int k(0); i < Nz; ++k) {
-                if (Cube[i][j][k].p_rosee < Cube[i][j][k].p_eau){
-                    Cube[i][j][k].nuage = true;
-                }
-            }
-        }
+
+bool CubedAir:: etat(){
+    bool nuage;
+    if (p_rosee() < p_eau()){
+        nuage = true;
     }
+    else {nuage = false;}
+    return nuage;
+
 }
